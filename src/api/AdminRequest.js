a44c427d84f69c3fba8b062f6d3af1feecf1f1cf
Beffin Yohannan <beffinyohannan25@gmail.com>
2023-02-03 14:23:56 +0530

    forth commit

--- src/api/AdminRequest.js
+++ src/api/AdminRequest.js
@@ -1,10 +1,11 @@
 import axios from '../api/Axios'
+import axiosJWT from './AuthAxios'
 
 export const adminLogin =(loginData)=>axios.post('/admin/login',loginData)
 
-export const enquiry = ()=>axios.get('/admin/dashboard')
+export const enquiry = ()=>axiosJWT.get('/admin/dashboard')
 
-export const deleteEnquiry =(id)=>axios.delete(`/admin/delete-enquiry/${id}`)
+export const deleteEnquiry =(id)=>axiosJWT.delete(`/admin/delete-enquiry/${id}`)
 
 export const logout =(refToken)=>axios.post('/admin/logout',{refToken})
 
